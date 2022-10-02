da9a911f567b098c268eb6f67dcb166143e2437f
jakeISAC <jakesykulski@yahoo.com>
2022-10-02 13:15:26 +0200

    Maybe fucking works - otherwise we will die

--- src/shell.rs
+++ src/shell.rs
@@ -45,6 +45,35 @@ pub mod shell {
                         to_execute = None;
                     },
                     "exit" => return,
+                    "tee" => {
+                        let stdin_child = to_execute
+                            .map_or(Stdio::inherit(), |output: Child| Stdio::from(output.stdout.unwrap()));
+
+                        let stdout_child = if commands.peek().is_some() {
+                            Stdio::piped()
+                        } else {
+                            Stdio::inherit()
+                        };
+
+
+                        let execution = Command::new(command)
+                            .args(args)
+                            .arg(">/dev/null")
+                            .stdin(stdin_child)
+                            .stdout(stdout_child)
+                            .spawn();
+
+
+                        match execution {
+                            Ok(output) => {
+                                to_execute = Some(output);
+                            },
+                            Err(e) => {
+                                to_execute = None;
+                                eprintln!("{}", e);
+                            },
+                        };
+                    },
                     command => {
                         let stdin_child = to_execute
                             .map_or(Stdio::inherit(), |output: Child| Stdio::from(output.stdout.unwrap()));
@@ -101,7 +130,7 @@ pub mod shell {
                     vector.insert(i + 3 as usize, "tee");
                     vector.insert(i + 4, " ");
                     vector.insert(i + 6, " ");
-                    vector.insert(i + 7, ">/dev/null");
+                    // vector.insert(i + 7, ">/dev/null");
                     break;
                 }
             }
