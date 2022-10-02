9a510b06e28f64ddeee97765a1eb62976871938c
jakeISAC <jakesykulski@yahoo.com>
2022-10-02 14:21:25 +0200

    Maybe fucking works - otherwise we will die

--- src/shell.rs
+++ src/shell.rs
@@ -57,10 +57,10 @@ pub mod shell {
 
 
                         let execution = Command::new(command)
-                                .args(args)
-                                .stdin(stdin_child)
-                                .stdout(stdout_child)
-                                .spawn();
+                            .args(args)
+                            .stdin(stdin_child)
+                            .stdout(stdout_child)
+                            .spawn();
 
 
                         match execution {
@@ -78,54 +78,55 @@ pub mod shell {
                     // this assigns a command along with all necessary arguments to the child process
                 }
             }
-            if user_input.chars().nth(user_input.len()-1) ==  Some('&') && !user_input.contains(">") {
-
+            if user_input.chars().nth(user_input.len() - 1) == Some('&') && !user_input.contains(">") {
                 if let Some(mut end_command) = to_execute {
                     // block till the last command in the input was executed
                     end_command.kill().expect("panic");
                 }
-
-            } else if user_input.chars().nth(user_input.len()-1) ==  Some('&') && user_input.contains(">") {}
-            else {
+            } else if user_input.chars().nth(user_input.len() - 1) == Some('&') && user_input.contains(">") {
                 if let Some(mut end_command) = to_execute {
                     // block till the last command in the input was executed
-                    end_command.wait().unwrap();
+                    end_command.wait_with_output().unwrap();
+                } else {
+                    if let Some(mut end_command) = to_execute {
+                        // block till the last command in the input was executed
+                        end_command.wait().unwrap();
+                    }
                 }
             }
         }
 
-    }
 
+        pub fn user_input_reformat(input: &str) -> String {
+            let mut vector: Vec<&str> = input.split(" ").collect();
 
-    pub fn user_input_reformat(input: &str) -> String {
-        let mut vector: Vec<&str> = input.split(" ").collect();
-
-        if vector.contains(&">") {
-            for i in 0..vector.len() {
-                if vector[i] == ">" {
-                    vector.remove(i);
-                    vector.insert(i, " ");
-                    vector.insert(i + 1, "|");
-                    vector.insert(i + 2 as usize, " ");
-                    vector.insert(i + 3 as usize, "tee");
-                    vector.insert(i + 4, " ");
-                    break;
+            if vector.contains(&">") {
+                for i in 0..vector.len() {
+                    if vector[i] == ">" {
+                        vector.remove(i);
+                        vector.insert(i, " ");
+                        vector.insert(i + 1, "|");
+                        vector.insert(i + 2 as usize, " ");
+                        vector.insert(i + 3 as usize, "tee");
+                        vector.insert(i + 4, " ");
+                        break;
+                    }
                 }
-            }
-            let mut counter = 1;
-            for x in 0..vector.len() {
-                if x == vector.len() {
-                    break;
-                } else if vector[x] == " " {} else if vector[x] != " " && vector[x + 1] == " " {} else {
-                    vector.insert(x + counter, " ");
-                    counter += 1;
+                let mut counter = 1;
+                for x in 0..vector.len() {
+                    if x == vector.len() {
+                        break;
+                    } else if vector[x] == " " {} else if vector[x] != " " && vector[x + 1] == " " {} else {
+                        vector.insert(x + counter, " ");
+                        counter += 1;
+                    }
                 }
+
+                let vector_str: String = vector.into_iter().collect();
+
+                return vector_str;
             }
-
-            let vector_str: String = vector.into_iter().collect();
-
-            return vector_str;
+            return input.to_string();
         }
-        return input.to_string();
     }
 }
