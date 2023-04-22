b5ad5f721dfd40bcf2df85504f198162cb86ea92
Noah <noah@langenheim.de>
2023-04-22 15:39:28 +0200

    Only used for calling shell_run()

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use crate::shell::shell::*;
 mod shell;
 mod utilities;
 
+// The logic for our shell is located in shell.rs. Here we only call the shell_run() function. 
+
 fn main(){
     shell_run();
 }
